0f7802b80b8c91db076cb9d5fac01cdc0e3a1504
John <j44.young@connect.qut.edu.au>
2020-09-07 16:39:55 +1000

    more updating front page

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ let selectDays = []
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Recent Vulnerabilities', 
-    title2: 'Check IP Address',
+    title2: 'Check a file',
     body: 'Enter the number of days',
-    body2: 'Second Body' });
+   });
 });
 
 module.exports = router;
