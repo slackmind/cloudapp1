bf73d04e321f329bbe1be7288a82380269cf0276
John <j44.young@connect.qut.edu.au>
2020-09-09 10:33:11 +1000

    search page

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,43 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios').default;
 
-let selectDays = []
+router.get('/search', function(req, res) {
+
+  console.log(req.query);
+  let days = req.query.days;
+  let startMonth = req.query.startMonth;
+  let endMonth = req.query.endMonth;
+  let startYear = req.query.startYear;
+  let endYear = req.query.endYear;
+
+  /* NIST CVE API URL  */
+  const NIST_URL = "https://services.nvd.nist.gov/rest/json/cves/1.0";
+  let nistTimeFrame = `?pubStartDate=${startYear}-${startMonth}-01T00:00:00:000 UTC-05:00`;
+  let numResults = '&resultsPerPage=5';
+
+  axios
+  .get(NIST_URL + nistTimeFrame + numResults)
+        
+        .then((response) => {
+          console.log('got here')
+          let { data } = response;
+          //console.log(data[0])
+          let allDescriptions = data.result.CVE_Items[0].cve.description.description_data.value;
+
+          console.log(allDescriptions);
+
+
+})
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
 
-  console.log(req.query);
-  let days = req.query.days;
-
-  
   res.render('index', { 
     title: 'Recent Vulnerabilities', 
     title2: 'Check a file',
+    //info: allDescriptions
    });
 });
 
